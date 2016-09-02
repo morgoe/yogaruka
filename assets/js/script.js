e26bd85dde96b9eeecda93500c74f5b6d7b99b48
Morgan Carter <morgancarter1@gmail.com>
2016-09-02 21:15:43 +0100

    add tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,6 +86,14 @@ $('.js-buyRetreat').click(function() {
 });	
 
 
+$('.tabs-link').click(function(e) {
+	e.preventDefault();
+	var target = $(this).attr('href');
+	$(this).parent().addClass('active').siblings().removeClass('active');
+	$(target).addClass('active').siblings().removeClass('active');
+});
+
+
 /* Instant Click */
 // InstantClick.init();
 
