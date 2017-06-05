5fef846b0018850fe9d957b3be8d0bb29e7097ad
Morgan Carter <morgancarter1@gmail.com>
2017-06-05 16:34:37 +1000

    add Crisp live chat

---
+++ assets/js/lib/crisp.js
@@ -0,0 +1,2 @@
+/* Crisp Live Chat */
+window.$crisp=[];window.CRISP_WEBSITE_ID="9727df14-5044-4a8f-83ec-7373b55d7548";(function(){ d=document;s=d.createElement("script"); s.src="https://client.crisp.im/l.js"; s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})(); 
